cc5b9dfa58d00386e24983f4c2673661dfe5378b
track <mateus.trackson@gmail.com>
2024-07-30 20:03:44 -0300

    Fazendo verificações

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
+
+const QUANTIDADE_CACHES = 4
+const EXIT = false
 
 const (
 	E = iota //0
@@ -21,7 +25,7 @@ type Cache struct { //pelo menos 5 posições.
 }
 
 type Linha struct {
-	Livros [4]Livro
+	Livros [5]Livro
 	Mesi   uint8
 }
 
@@ -42,4 +46,53 @@ type Processadores struct {
 func main() {
 	fmt.Println("Hello World!")
 
+	for {
+		var status bool = verificacao()
+		if !status {
+			return //quitar do sistema
+		}
+	}
+
+}
+
+func escolher_cache() {
+	var cache_escolhida string = strconv.Itoa(QUANTIDADE_CACHES)
+	//inválido por padrão.
+	fmt.Printf("\nQual cache você gostaria de usar? Selecione de 0 a %d\n", (QUANTIDADE_CACHES - 1))
+	fmt.Scan(&cache_escolhida)
+	cache_escolhida_int, err := strconv.Atoi(cache_escolhida)
+	for cache_escolhida_int >= QUANTIDADE_CACHES || cache_escolhida_int < 0 || err != nil {
+		fmt.Printf("Cache inexistente! Selecione uma cache válida, de 0 a %d\n", (QUANTIDADE_CACHES - 1))
+		fmt.Scan(&cache_escolhida)
+	}
+	fmt.Print(cache_escolhida)
+}
+
+func exit() bool {
+	var saida string = "2"
+	for {
+		fmt.Print("\nDeseja continuar operando no sistema (1) ou deseja finalizar (0)\n")
+		fmt.Scan(&saida)
+		//print(saida)
+		if saida != "0" && saida != "1" {
+			fmt.Print("ERRO! Selecione uma opção válida!")
+			saida = "2"
+			continue
+		} else if saida == "0" {
+			return false
+		} else {
+			return true
+		}
+	}
+}
+
+func verificacao() bool {
+	var estatos bool = exit()
+
+	if estatos {
+		escolher_cache()
+		return true
+	} else {
+		return false //não quer escolher.
+	}
 }
